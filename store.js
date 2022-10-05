5b978dbf2aa553afa2756e970cf2455834a5f404
relkimm <bbonggu92@gmail.com>
2022-10-05 23:43:07 +0900

    feat: getState, subscribe, dispatch 함수 기능 구현

---
+++ store.js
@@ -0,0 +1,29 @@
+function createStore() {
+  var state;
+  var listeners = [];
+
+  function getState() {
+    return state;
+  }
+
+  function subscribe(listener) {
+    listeners.push(listener);
+    return function unsubscribe() {
+      listeners.splice(listeners.indexOf(listener), 1);
+    };
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    listeners.forEach((listener) => {
+      listener();
+    });
+    return action;
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  };
+}
