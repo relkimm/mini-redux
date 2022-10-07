a6c688850c3a560b750e306e421bab8ce1d4d5a9
relkimm <bbonggu92@gmail.com>
2022-10-07 22:45:02 +0900

    feat: 튜토리얼 step4(applyMiddleware) 구현

--- step4.js
+++ step4.js
@@ -1,3 +1,77 @@
-function step4() {}
+function step4() {
+  var counterReducer = function (state, action) {
+    state = state ? state : 0;
+    switch (action.type) {
+      case "INCREASE":
+        return state + 1;
+      case "DECREASE":
+        return state - 1;
+      default:
+        return state;
+    }
+  };
+
+  var usernameReducer = function (state, action) {
+    state = state ? state : "";
+    switch (action.type) {
+      case "CHANGE":
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+
+  var reducer = combineReducers({
+    count: counterReducer,
+    username: usernameReducer,
+  });
+
+  var middleware = function (store) {
+    return function (next) {
+      return function (action) {
+        if (action.type === "INCREASE") {
+          alert("increased!!!");
+        } else if (action.type === "DECREASE") {
+          alert("decreased!!!");
+        }
+        var result = next(action);
+        return result;
+      };
+    };
+  };
+
+  var store = applyMiddleware(middleware)(createStore)(reducer);
+  store.subscribe(() => {
+    var state = store.getState();
+    document.querySelector("#step4 .count").innerHTML = state.count;
+    document.querySelector("#step4 .username").innerHTML = state.username;
+  });
+
+  var actionCreators = {
+    increase: function () {
+      return { type: "INCREASE" };
+    },
+    decrease: function () {
+      return { type: "DECREASE" };
+    },
+    change: function (payload) {
+      return { type: "CHANGE", payload };
+    },
+  };
+
+  var bounded = bindActionCreators(actionCreators, store.dispatch);
+
+  document.querySelector("#step4 .increase").addEventListener("click", () => {
+    bounded.increase();
+  });
+
+  document.querySelector("#step4 .decrease").addEventListener("click", () => {
+    bounded.decrease();
+  });
+
+  document.querySelector("#step4 input").addEventListener("input", (event) => {
+    bounded.change(event.target.value);
+  });
+}
 
 step4();
