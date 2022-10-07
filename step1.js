5c47c57e146c24408ba7c7b8d1da192fa81972d7
relkimm <bbonggu92@gmail.com>
2022-10-07 20:32:14 +0900

    feat: 튜토리얼 step1(createStore) 구현

--- step1.js
+++ step1.js
@@ -1,3 +1,32 @@
-function step1() {}
+const increaseAction = { type: "INCREASE" };
+const decreaseAction = { type: "DECREASE" };
+
+function step1() {
+  const countReducer = (state, action) => {
+    state = state ? state : 0;
+
+    switch (action.type) {
+      case "INCREASE":
+        return state + 1;
+      case "DECREASE":
+        return state - 1;
+      default:
+        return state;
+    }
+  };
+
+  const store = createStore(countReducer);
+  store.subscribe(() => {
+    document.querySelector("#step1 .count").innerHTML = store.getState();
+  });
+
+  document.querySelector("#step1 .increase").addEventListener("click", () => {
+    store.dispatch(increaseAction);
+  });
+
+  document.querySelector("#step1 .decrease").addEventListener("click", () => {
+    store.dispatch(decreaseAction);
+  });
+}
 
 step1();
